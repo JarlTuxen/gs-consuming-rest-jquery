59d853e0c46cf755dd9688929ccee63ec5598135
jarltuxen <jarl.tuxen@gmail.com>
2021-03-09 14:42:28 +0100

    local greeting service

--- complete/public/hello.js
+++ complete/public/hello.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
+    //alert("klar");
     $.ajax({
-        url: "http://rest-service.guides.spring.io/greeting"
+        url: "http://localhost:8080/greeting"
     }).then(function(data) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
